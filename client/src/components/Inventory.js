2ffce527e75d2f525f51563392eaf46d30b2758a
Daniel Nolan <DanNolan99@gmail.com>
2018-10-07 02:54:04 -0700

    adds trading interface

--- client/src/components/Inventory.js
+++ client/src/components/Inventory.js
@@ -3,6 +3,9 @@ import Sidebar from './Sidebar';
 import {web3} from './contract/web3util.js';
 import {Link} from 'react-router-dom';
 import { bytecode, abi } from './contract/NomadAsset.json';
+import {connect} from 'react-redux';
+import * as tradeActions from '../actions/tradeActions';
+import {createSignedOrder} from './contract/tradeUtils';
 
 const nomadAssetAddress = "0x519fdbb1f88cdd992f1624e255cbd7547235bea0";
 const contract = new web3.eth.Contract(abi);
@@ -12,9 +15,15 @@ class Inventory extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      items: []
+      items: [],
+      price: 0,
     }
+    this.postTrade = this.postTrade.bind(this);
+    this.handleChange = this.handleChange.bind(this);
     this.loadBalance()
+  }
+  handleChange(prop) {
+    return ({target}) => this.setState({[prop]: target.value});
   }
   loadBalance() {
     if (web3) {
@@ -23,16 +32,32 @@ class Inventory extends Component {
         if (account) {
           contract.methods.balanceOf(account).call({ from: account }).then((balance) => {
             if(balance > 0) {
-              this.setState({ items: [{
-                name: 'NOMAD Sword',
-                world: 'BrowserQuest',
-                description: 'It kills everything, 9000!!!!'
-              }] })
+              this.setState({
+                account,
+                items: [{
+                  name: 'NOMAD Sword',
+                  world: 'BrowserQuest',
+                  description: 'It kills everything, 9000!!!!'
+                }]
+              })
             }
           })
         }
       });
     }
+  }
+  postTrade() {
+    const { createTrade } = this.props;
+    const { price } = this.state;
+    createSignedOrder(this.state.account, nomadAssetAddress, 0).then((signedOrder) => {
+      createTrade({
+        item: this.state.items[0],
+        account: this.state.account,
+        signedOrder,
+        price
+      });
+      this.props.history.push(`/trading`);
+    });
   }
   render() {
     const { items } = this.state;
@@ -41,20 +66,25 @@ class Inventory extends Component {
         <Sidebar />
         <div className="inventory content">
           <div className="heading">
-            <h1> 
+            <h1>
               Check out your Items
-            </h1> 
+            </h1>
             <p>
               And see what world they are in!
             </p>
           </div>
-          { 
+          {
             items.map(({name, world, description}) => {
               return (
                 <div className="item">
-                  <div className="name"> {name} </div>
+                  <div className="header">
+                    <div className="title"> {name} </div>
+                  </div>
                   <div className="world"> Currently in world <Link to="/worlds">{world}</Link> </div>
                   <div className="description"> {description} </div>
+
+                  <input placeholder="Trade for ether" onChange={this.handleChange('price')}/>
+                  <div className="btn btn-primary" onClick={this.postTrade}> Post Trade </div>
                 </div>
               )
             })
@@ -65,4 +95,10 @@ class Inventory extends Component {
   }
 }
 
-export default Inventory;
+const mapDispatchToProps = dispatch => {
+    return({
+        createTrade: (trade) => dispatch(tradeActions.create(trade)),
+    });
+};
+
+export default connect(null, mapDispatchToProps)(Inventory);
--- client/src/store.js
+++ client/src/store.js
@@ -1,9 +1,22 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux';
+import tradeReducer from './reducers/tradeReducer';
+import {saveState, loadState} from './utilities/localStorageHelper';
 
 const reducer = combineReducers({
-
+  trading: tradeReducer
 });
 
-const store = createStore(reducer);
+// const persistedState = loadState();
+
+const store = createStore(
+  reducer,
+  // persistedState
+);
+
+store.subscribe(() => {
+    // const {trading} = store.getState();
+    // determines what is persisted in localstorage
+    // saveState({trading});
+});
 
 export default store;
