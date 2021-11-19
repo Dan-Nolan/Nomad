66970c6c16deb133ec84798f704b20147016cc8f
Dan <DanNolan99@gmail.com>
2021-11-19 15:16:34 -0500

    moves game to another repo

--- game/client/index.js
+++
@@ -1,4 +0,0 @@
-import "./index.scss";
-import state from "./state/index";
-
-state.initialize();
--- game/client/state/index.js
+++
@@ -1,6 +0,0 @@
-import Scene from './Scene';
-import GameState from './GameState';
-
-const state = new GameState(new Scene());
-
-export default state;
