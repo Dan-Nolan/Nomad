606f7e458cfc3e5ea82aaae226a1f4d3bcb76557
Dan <DanNolan99@gmail.com>
2021-11-21 02:16:40 -0500

    adds basic inventory items

--- client/src/content/Inventory.js
+++ client/src/content/Inventory.js
@@ -6,8 +6,8 @@ const IPFS_BASE = "https://ipfs.lukso.network/ipfs/";
 
 function Inventory() {
     const { profile: [profile] } = useContext(StoreContext);
-    
-    if(profile.loggedIn) {
+
+    if (profile.loggedIn) {
         const { name, profileImage: [profileImage] } = profile.LSP3Profile;
         const ipfsURL = IPFS_BASE + profileImage.url.slice(7);
 
@@ -18,7 +18,7 @@ function Inventory() {
                     <div className="info">
                         <div className="tag">
                             <div className="name">
-                                @{ name }
+                                @{name}
                             </div>
                             <div className="num">
                                 #6498
@@ -29,7 +29,64 @@ function Inventory() {
                         </h1>
                     </div>
                 </div>
-    
+
+                <div className="items">
+                    <div className="item">
+                        <div className="display-image">
+
+                        </div>
+                        <div className="info">
+                            <div className="header">
+                                Bow + Arrow
+                            </div>
+                            <div className="token-id">
+                                ID: #4156
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="item">
+                        <div className="display-image">
+
+                        </div>
+                        <div className="info">
+                            <div className="header">
+                                Health Potion
+                            </div>
+                            <div className="token-id">
+                                ID: #4884
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="item">
+                        <div className="display-image">
+
+                        </div>
+                        <div className="info">
+                            <div className="header">
+                                Extra Life Quarter
+                            </div>
+                            <div className="token-id">
+                                ID: #4321
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="item">
+                        <div className="display-image">
+
+                        </div>
+                        <div className="info">
+                            <div className="header">
+                                Air Jordan 4 Fire Red
+                            </div>
+                            <div className="token-id">
+                                ID: #1379
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
