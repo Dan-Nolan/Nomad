41316d6e9c320548c2b0322ac44a8fc0e9ba34af
Dan <DanNolan99@gmail.com>
2021-11-21 17:46:23 -0500

    adds static server

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require("path");
+const fs = require('fs');
+
+const app = express();
+const port = process.env.PORT || 3044;
+
+app.use(express.static('./client/build'));
+
+app.get('/', (_req, res) => {
+    const index = fs.readFileSync(path.join(__dirname, '../client/build/index.html'));
+    res.contentType("text/html");
+    res.send(index);
+});
+
+app.listen(port, () => {
+    console.log(`Up at port ${port}`);
+});
