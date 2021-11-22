1a4d919cc25601ac2f6bb2be1e3f4cf715368b94
Dan <DanNolan99@gmail.com>
2021-11-21 22:58:36 -0500

    use catchall for routing

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3044;
 
 app.use(express.static('./client/build'));
 
-app.get('/', (_req, res) => {
+app.get('*', (_req, res) => {
     const index = fs.readFileSync(path.join(__dirname, '../client/build/index.html'));
     res.contentType("text/html");
     res.send(index);
