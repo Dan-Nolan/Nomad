b0fc9f2a59cef599b8f91921d3a95580ec1e392a
Dan <DanNolan99@gmail.com>
2021-11-21 22:14:30 -0500

    adds integrations page

--- client/src/content/Integrations.js
+++ client/src/content/Integrations.js
@@ -1,4 +1,31 @@
 import "./Integrations.scss";
+import kingdom from "images/kingdom-image.jpg";
+import pacman from "images/pacman-image.jpg";
+import townsquare from "images/townsquare-image.jpg";
+import stardew from "images/stardew-image.jpg";
+
+const integrations = [{
+    name: "The Lost Kingdom",
+    img: kingdom,
+    description: "Check out how awesome this game is. Its got really cool stuff and all that jazz",
+    color: "#00511D",
+    link: ""
+}, {
+    name: "Stardew Valley",
+    description: "Check out how awesome this game is",
+    color: "#1160B9",
+    img: stardew
+}, {
+    name: "Pac-Man World",
+    description: "Check out how awesome this game is",
+    color: "#F60000",
+    img: pacman
+}, {
+    name: "Town Square",
+    description: "Check out how awesome this game is",
+    color: "black",
+    img: townsquare
+}];
 
 function Integrations() {
     return (
@@ -12,24 +39,23 @@ function Integrations() {
                 </h1>
             </div>
 
-            <div className="integration">
-                <div className="display-image">
-
-                </div>
-                <div className="info">
-                    <h1> Town Square </h1>
-                    <p>
-                        A description about the town square and how meta it is!
-                    </p>
-                    <div className="inventory">
-                        View My Inventory
+            {integrations.map(({ name, img, description, color }) => (
+                <div className="integration">
+                    <img src={img} alt={name} />
+                    <div className="info">
+                        <h1> {name} </h1>
+                        <p>
+                            {description}
+                        </p>
+                        <div className="inventory" style={{ "background-color": color }}>
+                            View My Inventory
+                        </div>
+                        <div className="proposals">
+                            Proposals
+                        </div>
                     </div>
-                    <div className="proposals">
-                        Proposals
-                    </div>
                 </div>
-            </div>
-
+            ))}
         </div>
     );
 }
