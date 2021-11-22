575982a4125d144ca6628a03c4ec16b190f09fe1
Dan <DanNolan99@gmail.com>
2021-11-22 02:10:07 -0500

    adds universe resource management

--- contracts/Nomad.sol
+++ contracts/Nomad.sol
@@ -25,4 +25,25 @@ contract Nomad {
         universe.tax = _tax;
         universe.governor = _governor;
     }
+
+    function createWorld(uint universeId, string calldata name, address owner) external {
+        require(msg.sender == universes[universeId].governor);
+        World storage world = universes[universeId].worlds.push();
+        world.name = name;
+        world.owner = owner;
+    }
+
+    function addResource(address resource, uint universeId, uint worldId) external {
+        require(msg.sender == universes[universeId].governor);
+        universes[universeId].resources[resource] = Resource(worldId);
+    }
+
+    function moveResource(uint universeId, address resource, uint destinationWorldId) external {
+        // TODO: check ownership in a standardized way 
+        // reqiure(resource.isOwner(msg.sender))
+
+        // TODO: incorporate resource tax
+
+        universes[universeId].resources[resource].worldId = destinationWorldId;
+    }
 }
