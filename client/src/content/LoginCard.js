aef6d854d3f59098dbabceeb3af604c240bcc079
Dan <DanNolan99@gmail.com>
2021-11-21 19:39:17 -0500

    login card

---
+++ client/src/content/LoginCard.js
@@ -0,0 +1,21 @@
+import Login from 'Login';
+import icon from "images/nomad-icon-login@2x.png";
+import "./LoginCard.scss";
+
+function LoginCard() {
+    return (
+        <div className="content">
+            <div className="login-card">
+                <div className="icon">
+                    <img src={icon} alt="icon" />
+                </div>
+                <div className="instructions">
+                    Please enter your Universal Profile address below:
+                </div>
+                <Login />
+            </div>
+        </div>
+    );
+}
+
+export default LoginCard;
